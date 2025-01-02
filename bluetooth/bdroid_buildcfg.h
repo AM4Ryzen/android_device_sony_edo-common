ade888ddfdc8b244a70acc6dc92ea2e0638118bd
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-01-02 08:55:51 -0600

    fix for name in bluetooth menu

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -28,7 +28,7 @@
 static inline const char* getBTDefaultName()
 {
     char device[PROPERTY_VALUE_MAX];
-    property_get("ro.boot.hardware", device, "");
+    property_get("ro.product.system.device", device, "");
 
     if (!strcmp("pdx203", device)) {
         return "Xperia 1 II";
